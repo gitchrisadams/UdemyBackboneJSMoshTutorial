b93c8667336cdd0836c63e185034df9f269e113d
Christopher Adams <chrismichaeladams@gmail.com>
2017-04-24 20:43:53 -0400

    Up to Models Project Sect 3 Lect 17

--- js/main.js
+++ js/main.js
@@ -1,5 +1,45 @@
+// Create a new model extending the Backbone Model:
+var Animal = Backbone.Model.extend({
+	// Define a walk function as an object of Animal:
+	walk: function(){
+		console.log("Animal walking...");
+	} 
+});
 
-// In the first few sections, we do all the coding here.
-// Later, you'll see how to organize your code into separate
-// files and modules.
-console.log('test');
+// Create a new Dog of type Animal:
+var Dog = Animal.extend();
+
+// Create a instance of a Dog:
+var dog = new Dog();
+
+// Call the walk method inherited from Animal on the dog object:
+dog.walk();
+
+// Create a Cat type extending Animal:
+var Cat = Animal.extend({
+	// Override the the Animals walk method:
+	walk: function(){
+		console.log("Cat walking...");
+	}
+});
+
+// Create an instance of a Cat:
+var cat = new Cat();
+
+// Call the walk method on the cat:
+cat.walk();
+
+// Create a Bird type extending Animal class:
+var Bird = Animal.extend({
+	walk: function(){
+		// Call the super class by using prototype and apply():
+		// So it will display 'Animal is walking'
+		Animal.prototype.walk.apply(this);
+	}
+});
+
+// Create an instance of a Bird:
+var bigBird = new Bird();
+
+// Call walk method on the bird.
+bigBird.walk();
